aac81447b192852b498174f4e626be084e01695d
Matt Kocubinski <mkocubinski@gmail.com>
2023-05-04 10:03:50 -0500

    Fix run-split-tests for bsd tools (macos)

--- sh/run-split-tests.sh
+++ sh/run-split-tests.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-go list ./... > pkgs.txt && split -d -n l/4 pkgs.txt pkgs.txt.part.
+set -o errexit
+
+go list ./... > pkgs.txt
+lines=`cat pkgs.txt | wc -l`
+length=$(( $lines / 4 ))
+split -d -l $length pkgs.txt pkgs.txt.part.
 
 #xargs --arg-file=pkgs.txt.part."$1" ruby -e 'p ARGV' go test -mod=readonly -timeout 30m -race -tags='cgo ledger test_ledger_mock' -count=1
 #xargs --arg-file=pkgs.txt.part."$1" -L 1 --interactive go test -mod=readonly -timeout 30m -race -tags='cgo ledger test_ledger_mock' -count=1
 
-xargs --arg-file=pkgs.txt.part."$1" go test -mod=readonly -timeout 30m -race -tags='norace ledger test_ledger_mock' -count=1
+cat pkgs.txt.part."$1" | xargs go test -mod=readonly -timeout 30m -race -tags='norace ledger test_ledger_mock' -count=1
 
 rm pkgs.txt*
